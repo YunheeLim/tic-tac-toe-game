95b637618fb103d621b80acee27b98f7590f4630
Yuni <yuni@Yuniui-MacBookPro.local>
2024-03-08 18:43:29 +0900

    Handle edit function during the game

--- src/App.jsx
+++ src/App.jsx
@@ -23,19 +23,22 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
-
-
 function App() {
+  const [players, setPlayers] = useState({
+    X: 'Player 1',
+    O: 'Player 2'
+  });
+
   const [gameTurns, setGameTurns] = useState([]);
   // const [hasWinner, setHasWinner] = useState(false);
   // const [activePlayer, setActivePlayer] = useState('X');
 
   const activePlayer = deriveActivePlayer(gameTurns);
-  
+
   // Rematch시 배열이 그대로인 오류 방지
   // js에서 배열은 같은 메모리를 참조하므로 새로운 배열 만들어야 함
   // deep copy
-  let gameBoard = [...initialGameBoard.map(array => [...array])]; 
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -55,8 +58,8 @@ function App() {
       firstSquareSymbol &&
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
-    ){
-      winner = firstSquareSymbol;
+    ) {
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -80,12 +83,31 @@ function App() {
     setGameTurns([]);
   }
 
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      };
+    })
+  }
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange}
+          />
         </ol>
         {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
         <GameBoard
